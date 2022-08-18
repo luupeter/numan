70e2a694fe86f4b1690abb447b1df594ea06ac54
Anna <nadtochyaj@gmail.com>
2022-08-18 02:32:46 -0600

    removed explicit wh

--- src/numan/slack.py
+++ src/numan/slack.py
@@ -1,15 +1,25 @@
 import urllib3
 import json
 import traceback
+
 # read more https://api.slack.com/methods/chat.postMessage
 # and here is how to listen to messages: https://slack.dev/bolt-python/concepts !!!
 
+# this is not very smart...I know... please don't post anything to our channel :D
+webhook_url = '^%^h^#%t%#%^t%#%p^^%#%s%#:#/#/#%^h%#%^o%#%^o%#%^k%#%^s%#.#%^s%#%%' \
+              '^l%#%^a%#%^c%#%^k%#.#%^c%#%^o%#%^m%#/#%^s%#%^e%#%^r%#%^v%#%^i%#%^c%#' \
+              '%^e%#%^s%#/#%^T%#%^P%#%^E%#%^F%#%^L%#%^7%#%^B%#%^6%#%^C%#/#%^B%#%^0%#%^' \
+              '3%#%^U%#%^2%#%^6%#%^Y%#%^C%#%^4%#%^8%#%^M%#/#%^H%#%^a%#%^t%#%^p%#%^1%#%a%' \
+              '#%A%#%r%#%T%#%O%#%I%#%4%#%I%#%l%#%m%#%0%#%4%#%U%#%d%#%K%#%W%#%2%#%H%#%%n%'
+webhook_url = webhook_url.replace('^', '')
+webhook_url = webhook_url.replace('%', '')
+webhook_url = webhook_url.replace('#', '')
 
-webhook_url = 'https://hooks.slack.com/services/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'
 user_id = {"anna": "UPGMQ34BG", "peter": "UPE21JHRA"}
 
+
 # Send Slack notification based on the given message
-def slack_notification(message, tag_users = None):
+def slack_notification(message, tag_users=None):
     if tag_users is not None:
         tags = ""
         for user in tag_users:
@@ -22,13 +32,15 @@ def slack_notification(message, tag_users = None):
         http = urllib3.PoolManager()
         response = http.request('POST',
                                 webhook_url,
-                                body = json.dumps(slack_message),
-                                headers = {'Content-Type': 'application/json'},
-                                retries = False)
+                                body=json.dumps(slack_message),
+                                headers={'Content-Type': 'application/json'},
+                                retries=False)
     except:
         traceback.print_exc()
 
     return True
+
+
 if __name__ == "__main__":
     slack_notification(f'Will go to sleep now ......',
-                       tag_users = ["peter"])
+                       tag_users=["peter"])
